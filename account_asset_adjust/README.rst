46c9ac6f7c32c9c9fbdb25f43e32fadc458796b8
Pedro A. Carpio <pcarpio@vauxoo.com>
2016-06-22 17:35:07 -0400

    [REF] account_asset_adjust: Redirecting Depreciation Board Compute Function.

--- account_asset_adjust/README.rst
+++ account_asset_adjust/README.rst
@@ -2,5 +2,5 @@
 ADJUSMENT TEST MODULE FOR ASSETS
 ================================
 
-The Following Module currently features Inflation-based Adjustment Tools to
- apply on an Asset's Gross Value and Depreciation.
+The Following Module currently features Inflation-based Adjustment Tools to 
+apply on an Asset's Gross Value and Depreciation.
--- account_asset_adjust/model/asset_adjust.py
+++ account_asset_adjust/model/asset_adjust.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # import time
 from datetime import datetime
-# from dateutil.relativedelta import relativedelta
+from dateutil.relativedelta import relativedelta
 
 from openerp import api, fields, models
 import openerp.addons.decimal_precision as dp
@@ -49,6 +49,114 @@ class AdjustableAsset(models.Model):
         'account.asset.adjust.initial',
         'asset_id', string="Initial Adjustment"
     )
+    
+    @api.v7
+    def compute_depreciation_board_ext(self, cr, uid, ids, context=None):
+        depreciation_lin_obj = self.pool.get('account.asset.depreciation.line')
+        currency_obj = self.pool.get('res.currency')
+        for asset in self.browse(cr, uid, ids, context=context):
+            if asset.value_residual == 0.0:
+                continue
+            posted_depreciation_line_ids = depreciation_lin_obj.search(
+                cr, uid, [
+                    ('asset_id', '=', asset.id), ('move_check', '=', True)
+                ],order='depreciation_date desc'
+            )
+            old_depreciation_line_ids = depreciation_lin_obj.search(
+                cr, uid, [
+                    ('asset_id', '=', asset.id), ('move_id', '=', False)
+                ]
+            )
+            if old_depreciation_line_ids:
+                depreciation_lin_obj.unlink(
+                    cr, uid, old_depreciation_line_ids,
+                    context=context
+                )
+
+            amount_to_depr = residual_amount = asset.value_residual
+            if asset.prorata:
+                depreciation_date = datetime.strptime(
+                    self._get_last_depreciation_date(
+                        cr, uid, [asset.id], context
+                    )[asset.id], '%Y-%m-%d'
+                )
+            else:
+                purchase_date = datetime.strptime(
+                    asset.purchase_date, '%Y-%m-%d'
+                )
+                #if we already have some previous validated entries,
+                #starting date is last entry + method period
+                if (len(posted_depreciation_line_ids)>0):
+                    last_depreciation_date = datetime.strptime(
+                        depreciation_lin_obj.browse(
+                            cr,uid,posted_depreciation_line_ids[0],
+                            context=context
+                        ).depreciation_date, '%Y-%m-%d'
+                    )
+                    depreciation_date = (
+                        last_depreciation_date
+                        + relativedelta(
+                            months=+asset.method_period
+                        )
+                    )
+                else:
+                    month = purchase_date.month
+                    common_period = [2,3,4,6]
+                    if (asset.method_period % 12 == 0):
+                        month = 1
+                    elif (asset.method_period in common_period):
+                        month -= (
+                            (purchase_date.month - 1)
+                            % asset.method_period
+                        )
+                    depreciation_date = datetime(
+                        purchase_date.year, month, 1
+                    )
+            day = depreciation_date.day
+            month = depreciation_date.month
+            year = depreciation_date.year
+            total_days = (year % 4) and 365 or 366
+
+            undone_dotation_number = self._compute_board_undone_dotation_nb(
+                cr, uid, asset, depreciation_date, total_days, context=context
+            )
+            for x in range(
+                len(posted_depreciation_line_ids), undone_dotation_number
+            ):
+                i = x + 1
+                amount = self._compute_board_amount(
+                    cr, uid, asset, i, residual_amount,
+                    amount_to_depr, undone_dotation_number,
+                    posted_depreciation_line_ids, total_days,
+                    depreciation_date, context=context
+                )
+                residual_amount -= amount
+                vals = {
+                    'amount': amount,
+                    'asset_id': asset.id,
+                    'sequence': i,
+                    'name': str(asset.id) +'/' + str(i),
+                    'remaining_value': residual_amount,
+                    'depreciated_value': (
+                        (asset.purchase_value - asset.salvage_value)
+                        - (residual_amount + amount)
+                    ),
+                    'depreciation_date': depreciation_date.strftime(
+                        '%Y-%m-%d'
+                    ),
+                }
+                depreciation_lin_obj.create(cr, uid, vals, context=context)
+                # Considering Depr. Period as months
+                depreciation_date = (
+                    datetime(year, month, day)
+                    + relativedelta(months=+asset.method_period)
+                )
+                day = depreciation_date.day
+                month = depreciation_date.month
+                year = depreciation_date.year
+        return True
+        
+
 
     @api.multi
     @api.depends('purchase_date','purchase_value')
@@ -85,3 +193,4 @@ class AdjustableAsset(models.Model):
                     "adjust_factor": adj_factor,
                     "adjust_value": adj_value,
                 })
+        return True
